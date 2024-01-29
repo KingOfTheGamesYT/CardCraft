15df079132083d614dad8e458f8f7af9e1ccf44b
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2024-01-29 00:59:27 -0500

    Forgot the main class
    main class made it to 1.20.1 too dw

--- src/main/java/com/animussurge/cardcraft/CardCraft.java
+++ src/main/java/com/animussurge/cardcraft/CardCraft.java
@@ -1,7 +1,9 @@
 package com.animussurge.cardcraft;
 
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -9,13 +11,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 @Mod("cardcraft")
-public class CardCraft
-{
+public class CardCraft {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "cardcraft";
 
@@ -25,12 +27,11 @@ public class CardCraft
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         MinecraftForge.EVENT_BUS.register(this);
-       // RegistryHandler.init();
+        RegistryHandler.init();
 
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
 
     }
 
@@ -43,12 +44,4 @@ public class CardCraft
     public void onServerStarting(ServerStartingEvent event) {
 
     }
-
-    public static final ItemGroup TAB = new ItemGroup("cardcraftbase") {
-
-        @Override
-        public ItemStack createIcon() {
-            return new ItemStack(RegistryHandler.card2spade.get());
-        }
-    };
 }
